5f845f0784f03eced29c79fd16dd7c7d0343a7af
Ian Mancini <ianmethyst@gmail.com>
2023-10-04 01:07:02 -0300

    improve mouse key speed settings

--- keyboards/sofle/keymaps/ametis70/config.h
+++ keyboards/sofle/keymaps/ametis70/config.h
@@ -6,6 +6,18 @@
 // Show wpm on right side
 #define SPLIT_WPM_ENABLE
 
+// Mouse key settings
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY 0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 64
+
 // Size optimizations
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
